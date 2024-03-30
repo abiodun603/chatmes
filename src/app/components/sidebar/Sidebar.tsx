794ccb8093a9a38aa3c6c4a967f95368b02460c7
abiodun603 <abioduncsc@gmail.com>
2024-03-30 22:24:52 +0100

    feat: sidebar, layout and navigation

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from "react";
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 async function Sidebar({ children } : { children: ReactNode}) {
+  const currentUser = await getCurrentUser(); 
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      <DesktopSidebar currentUser={currentUser!} />
+      <MobileFooter />
       <div className="lg:pl-20 h-full">
         {children}
       </div>

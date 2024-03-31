8f4ce796e099fda7637c29948eb727c4eb1cee4b
abiodun603 <abioduncsc@gmail.com>
2024-03-31 15:49:20 +0100

    feat: user screen: list all users

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,12 +1,18 @@
 import { ReactNode } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 export default async function UserLayout({
   children
 }: {children: ReactNode}) {
+  const users = await getUsers();
+  console.log(users)
+
   return (
     <Sidebar>
       <div className="h-full">
+        <UserList items={users} />
         {children}
       </div>
     </Sidebar>
